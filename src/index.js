cb605731f6a1ec84c33832da3e0583d1a4055b56
Jair <jairaceves56@gmail.com>
2024-08-15 09:32:04 -0600

    Establecimos la ruta para permitir CORS

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ const cors = require("cors")
 
 const app = express();
 
-// app.use(cors());
-// app.use(cors({
-//     origin: '*', // Or use '*' to allow all origins
-// }));
-
-// app.use(cors({
-    //     origin: 'https://ownersclub.stown.mx',
-    //     credentials: true, // Enable sending of credentials
-// }));
-app.use('*', cors()); // Handle preflight requests for all routes
+app.use(cors());
 app.use(express.json());
 
 
